ac15f779de4522d882f2331abdc99b59d567aad6
Tulgaa <enkh1218@gmail.com>
2020-08-24 19:01:00 +0800

    toglogciin eeljiig solidog bolno

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч,  нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -19,9 +19,37 @@ document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+// Шоог шидэх Eventlistener
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 доторх санамсаргүй тоо гаргаж авна
   var diceNumber = Math.floor(Math.random() * 6) + 1;
 
+  // Шооны зургийг вэб дээрээ гаргаж ирнэ
   diceDom.style.display = "block";
+  //Буусан санамсаргүй тоонд харгалзах шүүны зургийг вэб дээр гаргаж ирнэ
   diceDom.src = "dice-" + diceNumber + ".png";
+  // Буусан тоо нь 1ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ
+  if (diceNumber !== 1) {
+    // 1 ээс ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // 1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө
+
+    // Энэ тоглогчийн ээлжиндээ цуглуулсан оноог  0 болгоно
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    //Тоглогчийн ээлжиййг нөгөө тоглогч руу шилжүүлнэ
+    //Хэрэв идэвхтэй тоглогч 0 байвал идэвхтэй тоглогчийг 1 болго.
+    //Үгүй бол  идэвхтэй тоглогчийг 0 болго
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    //Улаан цэгийг шилжүүлэх
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    //Шоог түр алга болгоно
+    diceDom.style.display = "none";
+  }
 });
